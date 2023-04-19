2ad85861d334a44dc17937ecc91856828d9857b2
UWIMANAThierry <thierrybishop@gmail.com>
2023-04-19 12:10:10 +0200

    project set up

---
+++ src/components/HomePage.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>Hello User, Welcome on Home Page</h1>
+</template>
+
+<script>
+export default {
+    name : 'HomePage',
+
+    mounted() {
+    let user = localStorage.getItem("user-info");
+    if (!user) {
+      this.$router.push({ name: "SignUp" });
+    }
+  }
+}
+
+</script>
---
+++ src/components/LogIn.vue
@@ -0,0 +1,19 @@
+<template>
+  <img class="logo" src="../assets/logo.jpg" />
+  <h1>Login Page</h1>
+  <div class="register">
+    <input type="text" v-model="email" placeholder="Enter Email" />
+    <input type="password" v-model="password" placeholder="Enter Password" />
+    <button>Sign In</button>
+    <p>
+      Don't you have an account?
+      <router-link to="/sign-up">Sign Up </router-link>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LogIn'
+}
+</script>
