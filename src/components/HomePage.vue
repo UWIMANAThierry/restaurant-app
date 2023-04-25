08ebe341746123c8bef31df27e10efb2ffe59f6e
UWIMANAThierry <thierrybishop@gmail.com>
2023-04-25 15:53:08 +0200

    restaurant api

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -2,27 +2,48 @@
   <HeaderPage />
 
     <h1>Hello {{name}}, Welcome on Home Page</h1>
+    <table table="2">
+      <tr>
+      <td>Id</td>
+      <td>Name</td>
+      <td>Contact</td>
+      <td>Address</td>
+      </tr>
+      <tr v-for="item in restaurant" :key="item.id">
+        <td>{{ item.id }}</td>
+        <td>{{item.name}}</td>
+        <td>{{item.address}}</td>
+        <td>{{item.phone}}</td>
+        <td>{{item.email}}</td>
+      </tr>
+    </table>
 </template>
 
 <script>
 import HeaderPage from './HeaderPage.vue';
+import axios from 'axios';
 export default {
     name : 'HomePage',
     data(){
       return {
-        name:''
+        name:'',
+        restaurant:[]
       }
     },
     components: {
         HeaderPage
     },
 
-    mounted() {
+    async mounted() {
     let user = localStorage.getItem("user-info");
     this.name= JSON.parse(user).name;
     if (!user) {
       this.$router.push({ name: "SignUp" });
     }
+    let result = await axios.get("http://localhost:3000/restaurant");
+    console.warn(result)
+    this.restaurant = result.data;
+
   }
 }
 
