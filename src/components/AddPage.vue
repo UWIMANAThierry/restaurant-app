39fb8b7b392aea5e6e6ddfa01720b886323eec4c
UWIMANAThierry <thierrybishop@gmail.com>
2023-04-26 12:14:57 +0200

    add rertaurant

--- src/components/AddPage.vue
+++ src/components/AddPage.vue
@@ -1,20 +1,37 @@
 <template>
-  <HeaderPage  />
-  
-    <h1>Hello {{name}}, Welcome Add Restaurant Page</h1>
+<HeaderPage/>  
+<h1> Hello {{name}}, Welcome Add Restaurant Page </h1>
+<form class ="add">
+  <input type="text" name="name" placeholder="Enter Name" v-model="restaurant.name">
+  <input type="text" name="address" placeholder="Enter Address" v-model="restaurant.address">
+  <input type="text" name="contact" placeholder="Enter contact" v-model="restaurant.contact">
+  <input type="text" name="city" placeholder="Enter city" v-model="restaurant.city">
+  <button type="button" v-on:click="addRestaurant">Add new Restaurant</button>
+</form>
 </template>
 
 <script>
 import HeaderPage from './HeaderPage.vue';
 export default {
     name : 'AddPage',
-    data(){
-      return {
-        name:''
-      }
-    },
     components: {
         HeaderPage
+    },
+    data (){
+      return{
+        restaurant : {
+          name:'',
+          address:'',
+          contact:'',
+          city:''
+        }
+
+      }
+    },
+    methods :{
+      addRestaurant(){
+        this.$emit('addRestaurant',this.restaurant);
+      }
     },
 
     mounted() {
